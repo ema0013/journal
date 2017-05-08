7cafba079d42497169da4822dc0a5c4a84cd04ef
Student 6 <Student 6@10.8.33.97>
2017-05-08 12:06:24 -0400

    deleted excess

--- AES attempt/src/main/Main.java
+++ AES attempt/src/main/Main.java
@@ -1,9 +1,17 @@
 package main;
 
+import gui6.GUIApplication;
+
 public class Main extends GUIApplication{
 
 	public Main(int width, int height) {
 		super(width,height);
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
