35e434758c21d7566b348436a3973d48fa9cb37d
Student 6 <Student 6@10.8.33.97>
2017-06-05 13:12:29 -0400

    sds

--- AES attempt/src/main/Main.java
+++ AES attempt/src/main/Main.java
@@ -3,9 +3,20 @@ package main;
 //import aes.AES;
 import gui6.GUIApplication;
 
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
 public class Main extends GUIApplication{
 
@@ -19,16 +30,68 @@ public class Main extends GUIApplication{
 		setScreen(mainScreen);
 	}
 	public static void main(String[] args){
-//		Main g = new Main(800,600);
-//		Thread game = new Thread(g);
-//		game.start();
-		String textClar = "AES attempt to encrypt";
-		 String j = "1a25s8fe5dsg65ad";
-		 Cipher test = Cipher.getInstance("AES");
-		 Key keyTest = j.getBytes().getEncoded();
-		 System.out.println("Original Text: "+textClar);
-//		 byte[] encr = AES.aesEncrypt(textClar.getBytes(), j.getBytes());
-		 System.out.println("Encrypted text "+new String(encr));
+		//		Main g = new Main(800,600);
+		//		Thread game = new Thread(g);
+		//		game.start();
+		String text = "hello";
+		String key = "dAtAbAsE98765432"; // 128 bit key
+		byte[] encrypted = null;
+		
+		// Create key and cipher
+		Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+		Cipher cipher = null;
+		try {
+			cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// encrypt the text
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			encrypted = cipher.doFinal(text.getBytes());
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.err.println("Encrypted: " + new String(Base64.getEncoder().encodeToString(encrypted)));
+		
+		try {
+			cipher.init(Cipher.DECRYPT_MODE, aesKey);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			String decrypted = new String(cipher.doFinal(encrypted));
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
 	}
 
 }
