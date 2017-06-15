6196aa69e402fca7760796414ad3bf493102c13f
Edwin <Edwin@Lenovo-PCx.home>
2017-06-14 21:41:38 -0400

    i give up

--- AES attempt/src/main/AESScreen.java
+++ AES attempt/src/main/AESScreen.java
@@ -18,11 +18,13 @@ import guiTeacher.components.Action;
 import guiTeacher.components.Accordion;
 import guiTeacher.userInterfaces.Screen;
 import guiTeacher.components.Button;
+import guiTeacher.components.ClickableGraphic;
 import guiTeacher.components.Graphic;
 import guiTeacher.components.OptionAccordion;
 import guiTeacher.components.ScrollablePane;
 import guiTeacher.components.TextArea;
 import guiTeacher.components.TextBox;
+import guiTeacher.components.TextColoredLabel;
 import guiTeacher.components.TextField;
 import guiTeacher.components.TextLabel;
 import guiTeacher.userInterfaces.FullFunctionScreen;
@@ -31,14 +33,15 @@ import interfaces.Visible;
 import main.Main;
 
 public class AESScreen extends FullFunctionScreen implements MouseListener,KeyListener{
-	private TextArea title;
-	private Button toNotes;
+	private TextColoredLabel title;
+	private ClickableGraphic toNotes;
 	private Button encrypt;
 	private Button decrypt;
 	private Button copyOutput;
 	private TextField keyInput;
 	private TextField stateInput;
-	private TextLabel output; 
+	private TextColoredLabel output;
+	private TextColoredLabel whatThis;
 	private Graphic bg;
 	//private ScrollablePane cipherType;
 	
@@ -56,35 +59,54 @@ public class AESScreen extends FullFunctionScreen implements MouseListener,KeyLi
 	@Override
 	public void initAllObjects(List<guiTeacher.interfaces.Visible> viewObjects) {
 		bg = new Graphic(0, 0,getWidth(),getHeight(), "resources/test.png");
-		AESJavaX cipher = new AESJavaX();
-		title = new TextArea(300,50,200,100,"AES Cipher in Javax");
+		title = new TextColoredLabel(300,50,150,30,"AES Cipher in Javax", Color.BLUE, Color.yellow);
 		
-		toNotes = new Button(25,400,200,100,"Development Notes",Color.blue, new Action(){
+		toNotes = new ClickableGraphic(10,400,250,100,"resources/development arrow.png");
+		toNotes.setAction(new Action(){
 			
 			public void act(){
 				Main.g.setScreen(Main.development);
 			}
 		});
-		encrypt = new Button(200, 300, 60, 50, "Encrypt", Color.red, new Action(){
+		encrypt = new Button(50, 300, 60, 50, "Encrypt", Color.red, new Action(){
 			public void act(){
-				String test = cipher.encrypt(stateInput.getText(),keyInput.getText());
-				output.setText(test);
+				if(keyInput.getText().length()!=16){
+					output.setText("Please refrain from using keys that aren't of length 16.");
+				}
+				else{
+					String test = AESJavaX.encrypt(stateInput.getText(),keyInput.getText());
+					output.setText(test);
+				}
 			}
 		});
-		decrypt = new Button(270,300,60,50,"Decrypt",Color.red,new Action(){
+		decrypt = new Button(120,300,60,50,"Decrypt",Color.red,new Action(){
 			public void act(){
-				String test = cipher.decrypt(stateInput.getText(),keyInput.getText());
-				output.setText(test);
+				if(keyInput.getText().length()!=16){
+					output.setText("Please refrain from using keys that aren't of length 16.");
+				}
+				else{
+					String test = AESJavaX.decrypt(stateInput.getText(),keyInput.getText());
+					output.setText(test);
+				}
+				
 			}
 		});
-		copyOutput = new Button(340,300,100,50,"Copy Output",Color.blue,new Action(){
+		copyOutput = new Button(190,300,100,50,"Copy Output",Color.blue,new Action(){
 			public void act(){
 				stateInput.setText(output.getText());
 			}
 		});
-		stateInput = new TextField(50, 150, 350, 40, "Input in here");
-		keyInput = new TextField(50,200,350,40,"Key in here");
-		output = new TextLabel(100,100,400,100,"Output will be displayed here");
+		stateInput = new TextField(50, 200, 350, 40, "Input in here");
+		keyInput = new TextField(50,250,350,40,"Key in here");
+		output = new TextColoredLabel(50,100,400,100,"Output will be displayed here", Color.white, Color.black);
+		whatThis = new TextColoredLabel(470,50,300,400,
+				"The AES Cipher(Advanced Encryption Standard)"
+				+ "is a popular form of modern encryption that has many real life applications."
+				+ " It is a symmetric block cipher, meaning it has numerous blocks(mini-steps) which"
+				+ " will result in a final output. Try and create your own encrypted message for your friends to"
+				+ " crack!"
+				,Color.white,Color.black);
+		viewObjects.add(bg);
 		viewObjects.add(title);
 		viewObjects.add(toNotes);
 		viewObjects.add(stateInput);
@@ -93,6 +115,7 @@ public class AESScreen extends FullFunctionScreen implements MouseListener,KeyLi
 		viewObjects.add(encrypt);
 		viewObjects.add(decrypt);
 		viewObjects.add(copyOutput);
+		viewObjects.add(whatThis);
 	}
 
 	
--- AES attempt/src/main/NoteScreen.java
+++ AES attempt/src/main/NoteScreen.java
@@ -1,12 +1,18 @@
 package main;
 
+import java.awt.Color;
 import java.awt.event.MouseListener;
 import java.util.List;
 
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+import guiTeacher.components.TextArea;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class NoteScreen extends FullFunctionScreen implements MouseListener {
+	private TextArea notes;
+	private Button toAES;
 
 	public NoteScreen(int width, int height) {
 		super(width, height);
@@ -15,7 +21,19 @@ public class NoteScreen extends FullFunctionScreen implements MouseListener {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
+		notes = new TextArea(250,100,250,400,
+				"NOTICE: This implementation of the AES encryption Cipher is NOT "
+				+ "my own code. This has been recreated through the JavaX Cipher Class "
+				+ "and NOT through my own algorithm. I do not claim to own any of this code "
+				+ "nor do I intend on monetizing it. Any part of this program can be used by others "
+				+ "of free will w/o any chance of facing copyright infringment. Enjoy your day!");
+		toAES = new Button(200,450,100,70,"Return to AES Encryption Screen",Color.CYAN,new Action(){
+			public void act(){
+				Main.g.setScreen(Main.mainScreen);;
+			}
+		});
+		viewObjects.add(notes);
+		viewObjects.add(toAES);
 		
 	}
 
